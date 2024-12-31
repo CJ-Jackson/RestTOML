aa0671c478f46aa9c0746e43400d99313cac68df
Chris Jackson <hello@cjjackson.dev>
2024-12-31 14:04:58 +0000

    feat: started on RestTOML

---
+++ json/rest_toml_json.py
@@ -0,0 +1,9 @@
+#!/usr/env/bin python3
+import argparse
+
+parser = argparse.ArgumentParser(description="Process HTTP Rest request for JSON")
+
+parser.add_argument("toml")
+
+args = parser.parse_args()
+
