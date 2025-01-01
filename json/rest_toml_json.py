ee3ba435553fb2a2aca62982027a0322d87b640b
Chris Jackson <hello@cjjackson.dev>
2025-01-01 18:26:21 +0000

    feat: flatten list

--- json/rest_toml_json.py
+++ json/rest_toml_json.py
@@ -188,8 +188,17 @@ except TomlDataError as e:
     error_and_exit("TOML_DATA_ERROR", e.__str__())
 
 
+def _list_to_dict(v: list) -> dict:
+    d = {}
+    for i in range(len(v)):
+        d[str(i)] = v[i]
+    return d
+
+
 def _flatten_dict_gen(d, parent_key, sep):
     for k, v in d.items():
+        if type(v) is list:
+            v = _list_to_dict(v)
         new_key = parent_key + sep + k if parent_key else k
         if isinstance(v, MutableMapping):
             yield from flatten_dict(v, new_key, sep=sep).items()
